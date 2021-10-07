41e9821034bb93b60d1e3db0e47d16382bd4d89e
Sander Lathouwers <slathouw@fu-r9-p4.s19.be>
2021-10-07 12:17:59 +0200

    added bonus files

---
+++ srcs_bonus/server_bonus.c
@@ -0,0 +1,46 @@
+#include "../includes/minitalk.h"
+
+void	sig_handler(int sig, siginfo_t *siginfo, void *unused)
+{
+	static unsigned char	curr_char = 0x00;
+	static int				cnt = 0;
+	static pid_t			client_pid = 0;
+
+	(void)unused;
+	if (!client_pid)
+		client_pid = siginfo->si_pid;
+	curr_char |= (sig == SIGUSR1);
+	if (++cnt == 8)
+	{
+		cnt = 0;
+		if (curr_char == 0x00)
+		{
+			client_pid = 0;
+			return ;
+		}
+		ft_putchar_fd(curr_char, 1);
+		curr_char = 0x00;
+		kill(client_pid, SIGUSR1);
+	}
+	else
+	{
+		curr_char <<= 1;
+		kill(client_pid, SIGUSR2);
+	}
+}
+
+int	main(void)
+{
+	struct sigaction	sig_event;
+
+	ft_putstr_fd("Server PID [", 1);
+	ft_putnbr_fd(getpid(), 1);
+	ft_putstr_fd("]\n", 1);
+	sig_event.sa_flags = SA_SIGINFO;
+	sig_event.sa_sigaction = sig_handler;
+	sigaction(SIGUSR1, &sig_event, 0);
+	sigaction(SIGUSR2, &sig_event, 0);
+	while (1)
+		pause();
+	return (0);
+}
